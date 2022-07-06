2025f4bffc17f16f200560ed4fcea44e07924ff6
cclai02-test <108826004+cclai02-test@users.noreply.github.com>
2022-07-06 11:15:18 -0700

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,15 @@
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python3 palindrome.py [string]")
-    sys.exit(1)
-
-def Solution(s):
-    return s == s[::-1] and len(s) > 6
+class Solution:
+    # Write code below to complete prompt
+    def isPalindrome(self, s):
+        return s == s[::-1] and len(s) > 6
 
 def main():
-    print(Solution(sys.argv[1]))
+    tc1 = Solution()
+    inpyt = input()
+    # Write code below to complete prompt
+    print(tc1.isPalindrome(inpyt))
 
 if __name__ == "__main__":
     main()
